1d3415355aebddf88190db5d699a4104c3fa9a6e
shraddha <83664806+shraddhatech16@users.noreply.github.com>
2021-07-06 07:45:24 +0530

    multiply in c++.cpp

---
+++ multiply.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  float x, y;
+  float multiply;
+  
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  multiply = x * y;
+
+  cout << " The product of the numbers is: " << multiply;
+  return 0;
+}
